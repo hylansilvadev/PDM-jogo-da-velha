973cd6077025d251fd4bfbeaf4ae64d3ec340c74
Hylan Silva <hylansilva@outlook.com>
2024-03-19 14:37:17 -0300

    :tada: Iniciando a base do project :tada:

---
+++ app/src/main/java/com/example/tictactoe/AddPlayers.java
@@ -0,0 +1,46 @@
+package com.example.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_players);
+
+        EditText playerOne = findViewById(R.id.playerOne);
+        EditText playerTwo = findViewById(R.id.playerTwo);
+        Button startGameButton = findViewById(R.id.startGameButton);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getPlayerOneName = playerOne.getText().toString();
+                String getPlayerTwoName = playerTwo.getText().toString();
+
+                if(getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()) {
+                    Toast.makeText(AddPlayers.this, "Digite o nome dos jogadores!", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
